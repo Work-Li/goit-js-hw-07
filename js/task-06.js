d11cde6ea9dfceb1066cf5ab25746ed9600adbd0
Liliia Bakalinska <lili.lomaka@gmail.com>
2021-09-22 18:47:57 +0300

    добавляет решение 6 задачи

--- js/task-06.js
+++ js/task-06.js
@@ -1,14 +1,16 @@
 const inputEl = document.querySelector('#validation-input');
 
 const makeValidationOfInput = (event) => {
+ const dataLength = event.currentTarget.dataset.length;
+   
+        if (event.currentTarget.value.length === Number(dataLength)) {
+            event.currentTarget.classList.remove('invalid');
+            event.currentTarget.classList.add('valid');
+        } else {
+            event.currentTarget.classList.remove('valid');    
+            event.currentTarget.classList.add('invalid');
+        };
 
-    // Map( e =>
-        if (inputEl.value.length <= Number(inputEl.dataset.length)) {
-            inputEl.classList.add('#validation-input.valid')
-        } 
-        inputEl.classList.add('#validation-input.invalid');
-
-//         )
- } 
+}; 
 
 inputEl.addEventListener('change', makeValidationOfInput)
---
+++ js/task-07.js
@@ -0,0 +1,9 @@
+const controlEl = document.querySelector('#font-size-control');
+
+// const  resizes = (event) => {
+
+
+
+// };
+
+controlEl.addEventListener('mousemove', resizes);
